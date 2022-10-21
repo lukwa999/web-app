f9169d2182a9b9854fa6d73e0fc52b2b4bbae79f
lukwa999 <lukwa555@gmail.com>
2022-10-21 13:08:18 +0700

    first commit

---
+++ local/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import RestBasic from './rest-basic'
+import FormGet from './form-get'
+import FormPost from './form-post'
+
+export default function App(){
+  
+  return (
+    <div>
+    <RestBasic/>  
+    <FormGet/>
+    <FormPost/>
+    </div>
+  ) 
+}
